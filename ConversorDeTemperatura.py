047220495f764ebd42e240a3c7474857f9e1bf04
Wendel Davi <149116208+Wendel-Costa@users.noreply.github.com>
2025-11-22 00:06:23 +0000

    adicionando teste unitario

--- ConversorDeTemperatura.py
+++ ConversorDeTemperatura.py
@@ -1,14 +1,30 @@
 def conversorTemperatura(opcao, graus):
+    """
+    Converte temperaturas entre Celsius e Fahrenheit.
+
+    Testes:
+    >>> conversorTemperatura(1, 0)
+    32.0
+    >>> conversorTemperatura(1, 25)
+    77.0
+    >>> conversorTemperatura(2, 32)
+    0.0
+    >>> conversorTemperatura(2, 77)
+    25.0
+    >>> conversorTemperatura(3, 10)
+    'Opção inválida'
+    """
+
     if opcao == 1:
         f = graus * 1.8 + 32
-        print(f)
         return f
     
     elif opcao == 2:
         c = (graus - 32) / 1.8
-        print(c)
         return c
         
     else:
-        print("Opção inválida")
-        return 0
+        return "Opção inválida"
+
+import doctest
+doctest.testmod(verbose=True)
